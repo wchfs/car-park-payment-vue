284d13d1bec98ad34ec16129cb3706efa5203a3f
wch-kamil <kamil@webchefs.pl>
2022-08-06 15:24:57 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { init } from "@web3-onboard/vue";
+import injectedModule from "@web3-onboard/injected-wallets";
+import ConnectToWallet from "./components/GetUserLocation.vue";
+const injected = injectedModule();
+const infuraKey = "<INFURA_KEY>";
+const rpcUrl = `https://mainnet.infura.io/v3/${infuraKey}`;
+
+const web3Onboard = init({
+  wallets: [injected],
+  chains: [
+    {
+      id: "0x1",
+      token: "ETH",
+      label: "Ethereum Mainnet",
+      rpcUrl,
+    },
+  ],
+});
+</script>
+
+<template>
+  <h1>whateva</h1>
+  <ConnectToWallet></ConnectToWallet>
+</template>
+
+<style scoped></style>
---
+++ src/components/GetUserLocation.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="getUserLocation()"> connect </button>
+</template>
+<script>
+import { defineComponent } from 'vue'
+export default defineComponent({
+  props: {
+    message: String
+  },
+  methods: {
+    getUserLocation(){
+      navigator.geolocation.getCurrentPosition(this.setLocation, this.rejectLocation)
+    },
+    setLocation(loc){
+      console.log(loc);
+    },
+    rejectLocation(e){
+      console.log(e);
+    }
+  },
+  setup(props) {
+    props.message // type: string | undefined
+  }
+})
+</script>
+<style scoped>
+</style>
